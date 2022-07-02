45774b599cc7967fd8f2689fc23922ff7f538718
Anim Xhafa <animxhafa@gmail.com>
2022-07-02 14:33:32 -0400

    fix

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,15 +1,15 @@
 const multipart = require('parse-multipart')
 
 module.exports = async function (context, req) {
-context.log('Javascript HTTP function Week 2 parse-multipart')
+    context.log('Javascript HTTP function Week 2 parse-multipart')
 
-const boundary = multipart.getBoundary(req.headers['content-type'])
-const body = req.body
-const parts = multipart.Parse(body, boundary)
+    const boundary = multipart.getBoundary(req.headers['content-type'])
+    const body = req.body
+    const parts = multipart.Parse(body, boundary)
 
-let convertedResult = Buffer.from(parts[0].data).toString('base64')
+    let convertedResult = Buffer.from(parts[0].data).toString('base64')
 
-    context.res = {
-        body: convertedResult
-    }
+        context.res = {
+            body: convertedResult
+        }
 } 
